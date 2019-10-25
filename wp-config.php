f17ecea7bd5a5e0c736de07fd47601a38ed2abdc
ishanpradeep <ishanpradeepkumara@gmail.com>
2019-10-25 09:53:42 +0000

    latest

--- wp-config.php
+++ wp-config.php
@@ -64,7 +64,6 @@ define( 'NONCE_SALT',       'put your unique phrase here' );
  * a unique prefix. Only numbers, letters, and underscores please!
  */
 $table_prefix = 'wp_';
-
 /**
  * For developers: WordPress debugging mode.
  *
@@ -90,3 +89,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /** Sets up WordPress vars and included files. */
 require_once( ABSPATH . 'wp-settings.php' );
+
+define('DISALLOW_FILE_EDIT', true);
+define('DISALLOW_FILE_MODS', false);
